970733dc57e28f30f3231fb23c9b1930de2bf0b2
Dasidsheema <110417179+Dasidsheema@users.noreply.github.com>
2022-08-01 23:29:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = [94742416702]
+global.premium = [94742416702]
+global.ownernomer = 94742416702
+global.ownername = '🐦 REAL PAWANE TABA
+global.botname = DASIDU SHEEMAL
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = https://www.youtube.com/watch?v=cwkcRsucQtc
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
